a5f05938df1786a48f5679d2ac8cc74d31f5a042
Alberto <alberto.gglez@gmail.com>
2013-08-25 15:56:30 +0200

    Added GPLv3 license.

--- README.md
+++ README.md
@@ -8,8 +8,3 @@ Ingame:<br />
 Use the arrow keys to change the Snake direction.<br />
 Menu:<br />
 Use the arrow keys to move between the available options and the return key to select.
-
-ToDo
-========
-Add sounds to the game over screen.<br />
-Look for possible bugs or code optimizations.
--- gameScreen.h
+++ gameScreen.h
@@ -1,3 +1,23 @@
+/*
+    sFMLnake: A simple Snake clone made with SFML 2.
+    Copyright (C) 2013  Alberto García González
+
+    This file is part of sFMLnake.
+
+    sFMLnake is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    sFMLnake is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with sFMLnake.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef GAME_SCR_H
 #define GAME_SCR_H
 
